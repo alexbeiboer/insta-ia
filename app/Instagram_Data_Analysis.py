6440867ab9d07d2db38ab07cf1f46a1ab5b11460
Alex Beiboer <2020abeiboer@dwight.edu>
2019-10-24 16:42:29 -0400

    First commit

---
+++ app/Instagram_Data_Analysis.py
@@ -0,0 +1,119 @@
+# import imageio
+# imageio.plugins.ffmpeg.download()
+import numpy as np
+import time
+from datetime import datetime
+from InstagramAPI import InstagramAPI
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class Post:
+    def __init__(self, likes = None, year = None,month = None,day = None,hour = None,comment_count = None):
+        self.likes = likes
+        self.year = year
+        self.month = month
+        self.day = day
+        self.hour = hour
+        self.comment_count = comment_count
+
+
+def login(username, password):
+    #alexbeiboer
+    #Mosmanprep1
+    InstagramAPI_ = InstagramAPI(username, password)
+    InstagramAPI_.login()
+
+    InstagramAPI_.getProfileData()
+    result = InstagramAPI_.LastJson
+    user_name = result["user"]["username"]
+    # print (result["status"])
+    #print (result["user"]["username"])
+
+    #get timeline
+    InstagramAPI_.timelineFeed()
+    return InstagramAPI_
+
+def analyze_metrics(post_objects):
+    months = []
+    likess = []
+    comment_counts = []
+    for object in post_objects:
+        month = object.month
+        likes = object.likes
+        comment_count = object.comment_count
+        months.append(month)
+        likess.append(likes)
+        comment_counts.append(comment_count)
+    #format months
+    visitied = []
+    for i in range(len(months)):
+        month = months[i]
+        if month in visitied:
+            months[i] = int("1" + str(month))
+        visitied.append(months[i])
+
+
+    # df = pd.DataFrame({'likes': likess, "comment_counts":comment_counts},index = months)
+    # df=df.astype(float)
+    # lines = df.plot.line()
+    # # lines = df.plot.line(x=months, y=likess)
+    # fig = lines.get_figure()
+    # fig.savefig('/Users/2020shatgiskessell/Documents/Data_Conv2d8839/app/images/likes_months.png')
+
+    # fig = plt.figure()
+    # plt.plot(likess, 'r')
+    # plt.plot(months, 'g')
+    # fig.savefig('/Users/2020shatgiskessell/Documents/Data_Conv2d8839/app/images/likes_months.png')
+
+def get_metrics (posts):
+    likes = []
+    top_likers = []
+    post_objects = []
+    #print (posts[0])
+    for post in posts:
+        n_likes = post["like_count"]
+        comment_count = post["comment_count"]
+        date = post["taken_at"]
+        post_object = Post()
+        post_object.year = datetime.fromtimestamp(date).strftime('%Y')
+        post_object.month = datetime.fromtimestamp(date).strftime('%m')
+        post_object.day = datetime.fromtimestamp(date).strftime('%d')
+        post_object.hour = datetime.fromtimestamp(date).strftime('%H')
+        post_object.comment_count = post["comment_count"]
+        post_object.likes = n_likes
+
+        post_objecttop_likers = post["top_likers"]
+        likes.append(n_likes)
+        top_likers.append(post_objecttop_likers[0])
+        post_objects.append(post_object)
+    mean_likes = np.mean(likes)
+    #format top_likers
+    top_likers = list(set(top_likers))
+    top_likers_str = ""
+    for liker in top_likers:
+        top_likers_str += liker + ", "
+    return mean_likes, top_likers_str, post_objects
+
+def get_posts (InstagramAPI_):
+    myposts=[]
+    has_more_posts = True
+    max_id=""
+    while has_more_posts:
+        InstagramAPI_.getSelfUserFeed(maxid=max_id)
+        if InstagramAPI_.LastJson['more_available'] is not True:
+            has_more_posts = False #stop condition
+            #print("stopped")
+        max_id = InstagramAPI_.LastJson.get('next_max_id','')
+        myposts.extend(InstagramAPI_.LastJson['items'])
+        #lets server have time to make request
+        time.sleep(2)
+    return myposts
+
+# posts = get_posts ()
+# mean_likes, top_likers, post_objects = get_metrics (posts)
+# analyze_metrics(post_objects)
+# print ("mean number of likes: " + str(mean_likes))
+# print ("Top likers: ")
+# for liker in top_likers:
+#     print (liker)
---
+++ app/routes.py
@@ -0,0 +1,33 @@
+from flask import *
+from app import app,Instagram_Data_Analysis
+from flask import Flask, render_template, request
+import sys
+import os
+
+
+#
+# UPLOAD_FOLDER = os.path.basename('images')
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/display_data', methods=['GET', "POST"])
+def display_data():
+    user_data = dict (request.form)
+    username = user_data["username"]
+    password = user_data["password"]
+    #login with username and password entered into form
+    InstagramAPI_ = Instagram_Data_Analysis.login(username, password)
+    #get posts
+    posts = Instagram_Data_Analysis.get_posts(InstagramAPI_)
+    # #get post metric
+    mean_likes, top_likers, post_objects = Instagram_Data_Analysis.get_metrics(posts)
+    print (top_likers)
+    # #analyse metrics
+    Instagram_Data_Analysis.analyze_metrics(post_objects)
+    #full_filename = os.path.join(app.config['UPLOAD_FOLDER'], '/Users/2020shatgiskessell/Documents/Data_Conv2d8839/app/images/likes_months.png')
+    return render_template('display_data.html', output_filepath = "../static/likes_months.png", avg_likes = "Mean number of likes: " + str(int(mean_likes)), top_likers = top_likers)
+    #return render_template('display_data.html')
